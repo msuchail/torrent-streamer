5013c710e759840b0b30188f7d410fdfd63a83db
mathias <mathias.suchail@yahoo.com>
2024-11-05 20:50:32 +0100

    fix dark mode

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,7 +13,7 @@
         .bg-dots{background-image:url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(200,200,255,0.15)'/%3E%3C/svg%3E");
     </style>
 
-    <div class="relative min-h-screen bg-gray-100 bg-center bg-dots dark:bg-gray-900 selection:bg-indigo-500 text-white">
+    <div class="relative min-h-screen bg-center bg-dots bg-gray-900 selection:bg-indigo-500 text-white">
         <header>
             <div class="justify-end pt-3 container mx-auto flex flex-col gap-5 pb-5">
                 <nav class="flex justify-between items-center">
